7e9da3d45d33d6d2eb4049bbdc7f799c3cb2e316
NjengaC <victorcyrus01@gmail.com>
2024-03-06 19:58:47 +0300

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,24 +7,8 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ['54.144.140.209', '34.202.233.3']
-env.user = 'ubuntu'
-
-
-def do_pack():
-    """
-        Return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
+env.hosts = ["54.144.140.209", "34.202.233.3"]
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
@@ -38,7 +22,6 @@ def do_deploy(archive_path):
             archive_filename[:-4])
 
         put(archive_path, "/tmp/")
-
 
         run("sudo mkdir -p {}".format(version_folder))
         run("sudo tar -xzf /tmp/{} -C {}/".

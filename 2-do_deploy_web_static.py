c54dc6900a7e1712586f99af46fef9cf5e0e4773
NjengaC <victorcyrus01@gmail.com>
2024-03-07 00:53:40 +0300

    kama hii imekataa ni sawa tu

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ import os
 env.hosts = ['54.144.140.209', '34.202.233.3']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
+env.warn_only = True
 
 
 def do_deploy(archive_path):
@@ -18,21 +19,26 @@ def do_deploy(archive_path):
     if os.path.isfile(archive_path) is False:
         return False
     try:
+
         archive = archive_path.split("/")[-1]
         path = "/data/web_static/releases"
         put("{}".format(archive_path), "/tmp/{}".format(archive))
         folder = archive.split(".")
-        run("sudo mkdir -p {}/{}/".format(path, folder[0]))
-        new_archive = '.'.join(folder)
-        run("sudo tar -xzf /tmp/{} -C {}/{}/"
-            .format(new_archive, path, folder[0]))
-        run("sudo rm /tmp/{}".format(archive))
-        run("sudo mv -f {}/{}/web_static/* {}/{}/"
-            .format(path, folder[0], path, folder[0]))
-        run("sudo rm -rf {}/{}/web_static".format(path, folder[0]))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf {}/{} /data/web_static/current"
-            .format(path, folder[0]))
+        if run("test -d {}/{}/".format(path, folder[0])).failed:
+            run("sudo mkdir -p {}/{}/".format(path, folder[0]))
+            new_archive = '.'.join(folder)
+            run("sudo tar -xzf /tmp/{} -C {}/{}/"
+                .format(new_archive, path, folder[0]))
+            run("sudo rm /tmp/{}".format(archive))
+            run("sudo mv -f {}/{}/web_static/* {}/{}/"
+                .format(path, folder[0], path, folder[0]))
+            run("sudo rm -rf {}/{}/web_static".format(path, folder[0]))
+            run("sudo rm -rf /data/web_static/current")
+            run("sudo ln -sf {}/{} /data/web_static/current"
+                .format(path, folder[0]))
+        else:
+            run("sudo rm /tmp/{}".format(archive))
         return True
+
     except Exception:
         return False

b12e2ce1f7a0d7646444e9bd39189c8639c69a64
NjengaC <victorcyrus01@gmail.com>
2024-03-07 12:43:36 +0300

    print("New version deployed!)

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,6 +33,7 @@ def do_deploy(archive_path):
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -sf {}/{} /data/web_static/current"
             .format(path, folder[0]))
+        print("New version deployed!")
         return True
     except Exception:
         return False

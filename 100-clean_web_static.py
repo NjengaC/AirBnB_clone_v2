4cc26435602d2d15ca64de4b3860cea11916fb6f
NjengaC <victorcyrus01@gmail.com>
2024-03-09 01:24:04 +0300

    check

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Function that deploys """
 from fabric.api import *
-
+from fabric.operations import run, local
 env.hosts = ['54.144.140.209', '34.202.233.3']
 env.user = "ubuntu"
 

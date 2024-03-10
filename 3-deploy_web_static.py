da736f44dffaa287d14a1c4b109ead6e17ddc403
NjengaC <victorcyrus01@gmail.com>
2024-03-10 11:03:27 +0300

    state

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,65 +1,98 @@
 #!/usr/bin/python3
-""" Fabric script to generate a .tgz archive from web_static """
+"""
+This script generates a .tgz archive from the web_static folder
+and distributes it to web servers using Fabric.
+"""
 
-from datetime import datetime
+
 from fabric.api import *
-from fabric.operations import run, put, sudo
-import os
+from fabric.api import runs_once
+from fabric.api import env
+from fabric.api import local
+from os.path import exists
+from datetime import datetime
 
 env.hosts = ["54.144.140.209", "34.202.233.3"]
 env.user = "ubuntu"
-env.key_filename = "~/.ssh/id_rsa"
 
 
-@runs_once
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder
+    Generates a .tgz archive from the web_static folder.
+
+    Returns:
+        str: Archive path if archive has been generated, otherwise None.
     """
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    archive_name = "web_static_{}.tgz".format(current_time)
+
+    archive_path = "versions/{}".format(archive_name)
+
+    print("Packing web_static to {}".format(archive_path))
+
     local("mkdir -p versions")
 
-    now = datetime.now()
-    file_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-
-    result = local("tar -cvzf versions/{} web_static".format(file_name))
+    result = local("tar -cvzf {} web_static".format(archive_path))
 
     if result.succeeded:
-        return "versions/{}".format(file_name)
+        print("web_static packed: {} ".format(archive_path))
+        return archive_path
     else:
+        print("Failed to pack web_static")
         return None
 
 
 def do_deploy(archive_path):
-    """
-    distributes an archive to web servers, using the function do_deploy
-    """
-    if os.path.isfile(archive_path) is False:
+    """ Distributes an archive to webserver """
+
+    if not exists(archive_path):
         return False
-    try:
-        archive = archive_path.split("/")[-1]
-        path = "/data/web_static/releases"
-        put("{}".format(archive_path), "/tmp/{}".format(archive))
-        folder = archive.split(".")
-        run("sudo mkdir -p {}/{}/".format(path, folder[0]))
-        new_archive = '.'.join(folder)
-        run("sudo tar -xzf /tmp/{} -C {}/{}/ --strip-components=1"
-            .format(new_archive, path, folder[0]))
-        run("sudo rm /tmp/{}".format(archive))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf {}/{} /data/web_static/current"
-            .format(path, folder[0]))
-        print("New version deployed!")
-        return True
-    except Exception:
+
+    print("Deploying new version")
+
+    archive_name = archive_path.split("/")[-1]
+    folder_name = archive_name[: -4]
+    dir_path = "/data/web_static/releases/{}".format(folder_name)
+
+    put(archive_path, "/tmp/")
+    run("mkdir -p {}".format(dir_path))
+    result = run("tar -xzf /tmp/{} -C {}".format(archive_name, dir_path))
+
+    if result.failed:
         return False
+
+    run("cp -r {}/web_static/* {}".format(dir_path, dir_path))
+    run("rm -rf /tmp/{} {}/web_static".format(archive_name, dir_path))
+    run("rm -rf /data/web_static/current")
+    run("ln -s {} /data/web_static/current".format(dir_path))
+
+    print("New version deployed!")
+
+    return True
 
 
 def deploy():
     """
-    Create and archive and get its path
+    Main deployment function.
+    Creates and deploys a new version locally.
     """
-    archive_path = do_pack()
-    if archive_path is None:
+    local_archive_path = do_pack()
+    if not local_archive_path:
+        print("Failed to create archive.")
         return False
-    deploy_res = do_deploy(archive_path)
-    return deploy_res
+
+    print("Local archive created successfully : {}".format(local_archive_path))
+
+    extract_path = "/tmp/"
+    local("mkdir -p {}".format(extract_path))
+    local("tar -xzf {} -C {}".format(local_archive_path, extract_path))
+
+    local("rm -rf /data/web_static/current")
+    local("ln -s {}/web_static /data/web_static/current".format(extract_path))
+
+    print("Deployment successful!")
+    return True
+
+
+deploy()

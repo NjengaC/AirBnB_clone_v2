1c004468e739ef4a274f09310ec47362a697bd6d
NjengaC <victorcyrus01@gmail.com>
2024-03-23 01:51:10 +0300

    flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
+"""Start web application with two routings
 """
-Starts a Flask web application.
-"""
-from flask import Flask, render_template, abort
+
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states_list(id=None):
+    """Render template with states
+    """
+    path = '9-states.html'
+    states = storage.all(State)
+    return render_template(path, states=states, id=id)
 
 
 @app.teardown_appcontext
 def app_teardown(arg=None):
-    """close the current session"""
+    """Clean-up session
+    """
     storage.close()
 
 
-@app.route("/states")
-def states():
-    """Displays page with a list of all States """
-    states = storage.all(State)
-    return render_template("9-states.html", state=states)
-
-
-@app.route("/states/<id>")
-def states_id(id):
-    """Display page with state and its cities if id is passes """
-    for state in storage.all(State).values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)

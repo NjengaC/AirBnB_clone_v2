dfd184006dda89c909b60a127af54a8eb6cfb0f5
NjengaC <victorcyrus01@gmail.com>
2024-03-22 11:21:53 +0300

    main

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 """ Function that deploys """
 from fabric.api import env
-from fabric.operations import run, local
+from fabric.operations import run, local, sudo
+from fabric.context_managers import lcd, cd
+
 env.hosts = ['54.144.140.209', '34.202.233.3']
 env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """ CLEANS """
+    """ Clean up some archive """
 
     number = int(number)
-
-    if number == 0:
-        number = 2
-    else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs sudo rm -rf'.format(path, number))
+
+    if number == 0 or number == 1:
+        number = 1
+    else:
+        number -= 1
+
+    with cd("{}".format(path)):
+        sudo("ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number + 1)))
+
+    with lcd("versions"):
+        local("ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number + 1))
+
+
+#    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+#    path = '/data/web_static/releases'
+#    run('cd {} ; ls -t | tail -n +{} | xargs sudo rm -rf'.format(path, number))
---
+++ safe/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Route to display "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+# Route to display "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+# Route to display "“C ” followed by the value of the text variable"
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+
+# Route to display "“Python ” followed by the value of the text variable"
+# Default text is cool
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_route(text="is cool"):
+    text = text.replace('_', ' ')
+    return 'Python ' + text
+
+
+# Route to display "“n is a number" ONLY if n is an integer"
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    return  "{} is a number".format(n)
+
+
+# Route to display a HTML page ONLY if n is an integer"
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_temp(n):
+    return render_template("5-number.html", n=n)
+
+
+# Route to display a HTML page ONLY if n is an integer"
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_temp(n):
+    return render_template("5-number.html", n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)

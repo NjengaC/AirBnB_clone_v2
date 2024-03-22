d4cc98318307e4ebefe90173af9a8173ea5180e6
NjengaC <victorcyrus01@gmail.com>
2024-03-22 13:15:44 +0300

    no class?

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,6 +6,8 @@ This is 7-states_list.py Module Documentation
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from sqlalchemy.orm import sessionmaker, scoped_session
+
 app = Flask(__name__)
 
 
@@ -13,7 +15,7 @@ app = Flask(__name__)
 def states_list():
     """Display a list of all State objects"""
     states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda state: state.name)
+    states_sorted = sorted(states, key=lambda s: s.name)
     return render_template('7-states_list.html', states=states_sorted)
 
 
@@ -24,4 +26,4 @@ def teardown_appcontext(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)

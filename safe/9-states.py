6c4edcc9b3219a8aab5e434fadfaf94d2bdbfb60
NjengaC <victorcyrus01@gmail.com>
2024-03-23 03:00:12 +0300

    check t

--- safe/9-states.py
+++ safe/9-states.py
@@ -6,23 +6,22 @@ from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
+def app_teardown(error):
     """close the current session"""
     storage.close()
 
 
-@app.route("/states")
+@app.route("/states", strict_slashes=False)
 def states():
     """Displays page with a list of all States """
     states = storage.all(State)
     return render_template("9-states.html", state=states)
 
 
-@app.route("/states/<id>")
+@app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
     """Display page with state and its cities if id is passes """
     for state in storage.all(State).values():

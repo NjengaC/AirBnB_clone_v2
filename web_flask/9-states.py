5c764b895a824b7257289729b9c4064a6800564f
NjengaC <victorcyrus01@gmail.com>
2024-03-23 13:43:34 +0300

    import city

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,7 +5,7 @@ Starts a Flask web application.
 from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
-# from models.city import City
+from models.city import City
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 

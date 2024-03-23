c34e4430f7b48d7588a9585e085b2bb1566fb5ac
NjengaC <victorcyrus01@gmail.com>
2024-03-23 13:22:21 +0300

    now or now

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,8 +5,9 @@ Starts a Flask web application.
 from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
+from models.city import City
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -15,14 +16,16 @@ def app_teardown(arg=None):
     storage.close()
 
 
-@app.route("/states")
-def states():
-    """Displays page with a list of all States """
-    states = storage.all(State)
+@app.route("/states", strict_slashes=False)
+def _states():
+    """
+    Displays page with a list of all States
+    """
+    states = storage.all('State')
     return render_template("9-states.html", state=states)
 
 
-@app.route("/states/<id>")
+@app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
     """Display page with state and its cities if id is passes """
     for state in storage.all(State).values():

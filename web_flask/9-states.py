3cc91ef76fe56811d6a50314839262b68b7cd284
NjengaC <victorcyrus01@gmail.com>
2024-03-23 14:28:19 +0300

    jumanji

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,9 +12,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def app_teardown(arg=None):
-    """
-    close the current session
-    """
+    """close the current session """
     storage.close()
 
 
